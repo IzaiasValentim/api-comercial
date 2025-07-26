6e1d0f5529e7b65f4028efd4cab177ac2e9a39a8
Izaias Valentim <izaias451@gmail.com>
2025-07-26 17:59:18 -0300

    fiz/ Remoção de entidades fora de escopo da entrega.

--- src/main/java/com/izaiasvalentim/general/Repository/CompanhiaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.izaiasvalentim.general.Repository;
-
-import com.izaiasvalentim.general.Domain.Companhia;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CompanhiaRepository extends JpaRepository<Companhia, Integer> {
-}
