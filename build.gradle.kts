0267a3784a1b932e6fe6481217d0cebb40a7aacf
luis.izaias <izaias451@gmail.com>
2025-11-25 10:46:17 -0300

    update/ Atualização das configurações para execução do projeto em desenvolvimento.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.4.1"
     id("io.spring.dependency-management") version "1.1.7"
+    kotlin("jvm")
 }
 
 group = "com.IzaiasValentim"
@@ -23,7 +24,7 @@ dependencies {
     implementation("org.springframework.security:spring-security-core")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    //runtimeOnly("com.h2database:h2")
+    runtimeOnly("com.h2database:h2")
     // https://mvnrepository.com/artifact/org.postgresql/postgresql
     implementation("org.postgresql:postgresql:42.7.3")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -31,6 +32,7 @@ dependencies {
 
     implementation("me.paulschwarz:spring-dotenv:4.0.0")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.withType<Test> {
